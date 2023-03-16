abdfd45bd35aa69f830dd95b2d87c147b3484556
Crown384 <crownstephen384@gmail.com>
2023-03-16 23:07:55 +0100

    suuwiwiw

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
-int main(void)
+int main(void) /*This is the main functuon and is void
+		  *It returns a value of 0 il00
+		  */
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return 0;
