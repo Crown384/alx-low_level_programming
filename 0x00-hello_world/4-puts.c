9dc9e6694fb3280f91e5044f3a52caaf834a6917
Crown384 <crownstephen384@gmail.com>
2023-03-17 04:23:06 +0100

    updated y4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 int main(void)
-	/*This is the main functuon and
-		 *is void It returns a value of il00 
-		 */
+	/*
+	 *is void It returns a value of il00
+	 */
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
