e756513998400d7a1193bb2b05c2330e75912bbd
Crown384 <crownstephen384@gmail.com>
2023-03-18 05:08:25 +0100

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main- Prints all possible ckmbinations of letter 0-9 separted by , the space. no printf no puts
+ * Return: Always 0 {Success)
+ *
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+	putchar(i);
+	if (i != '9')
+	}
+	putchar(',');
+	putchar(' ')
+	}
+	}
+	putchar('\n');
+	return (0);
+}
