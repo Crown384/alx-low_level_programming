7cf1a5838490372b42f4eebe1d6fc7ad50fbe4fd
Crown384 <crownstephen384@gmail.com>
2023-03-18 04:21:59 +0100

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main- Prints each single unit of base 1
+ * Return: Always 0 success
+ *
+ */
+
+int main(void)
+{
+	int len;
+
+	for (len = 0; len <= 9; len++)
+	printf("%d", len);
+	printf("\n");
+	return (0);
+}
+
