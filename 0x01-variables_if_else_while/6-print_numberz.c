99f8dbc30119d23368c0c2127d4e7b2236024bd8
Crown384 <crownstephen384@gmail.com>
2023-03-18 04:33:58 +0100

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main- prints int 0-9 w8thiit using printf or puts fx
+ * Return: Always 0 success
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	putchar(i);
+	putchar('\n');
+	return (0);
+}
