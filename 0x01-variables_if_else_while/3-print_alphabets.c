7009a484db9d3dc95c65091bf711b1119a9856db
Crown384 <crownstephen384@gmail.com>
2023-03-18 03:42:42 +0100

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+
+/*
+ * main- Prints low() then upper()
+ * Return: Always(success)
+ *
+ */
+
+int main(void)
+{
+	char len;
+
+	for(len = 'a';len <= 'z';len ++)
+	putchar(len);
+
+	for(len = 'A';len <= 'Z';len ++)
+        putchar(len);
+	putchar('\n');
+}
