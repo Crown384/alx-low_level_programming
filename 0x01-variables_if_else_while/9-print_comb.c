b924212ef6c95370160eaf43d762cf27c82f377d
Crown384 <crownstephen384@gmail.com>
2023-03-18 05:52:47 +0100

    jskks

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 
 /**
- * main- Prints all possible ckmbinations of letter 0-9 separted by , the space. no printf no puts
+ * main- Prints all possible ckmbinations of letter 0-9 
  * Return: Always 0 {Success)
  *
  */
