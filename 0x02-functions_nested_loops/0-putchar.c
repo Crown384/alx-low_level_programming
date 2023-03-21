e405d63f8651fbce96a3b2781fd72222396dca9d
Crown384 <crownstephen384@gmail.com>
2023-03-21 20:29:42 +0100

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,11 @@
 #include <unistd.h>
 
 /**
-*_putchar - written tyevcharacter c to stdout
-*@c: the character to print
-*Return: On success 1
-*On error: -1 is returened and error is set appropraitely
-*/
+ * _putchar - written tyevcharacter c to stdou
+ * @c: the character to print
+ * Return: On success 1
+ * On error: -1 is returened and error is set appropraitel
+ */
 
 
 int _putchar(char c)
