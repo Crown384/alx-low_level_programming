eea8c461c23173cccc80562027c578a9f5bb8a33
Crown384 <crownstephen384@gmail.com>
2023-03-21 21:13:04 +0100

    1-alphabet.c & main.sh

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main- Prints with sekf created funxtuona
+ * Return: Always 
+ */
+
+#include "main.h"
+
+
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+	_putchar(c);
+	}
+	_putchar('\n');
+}
+
+
