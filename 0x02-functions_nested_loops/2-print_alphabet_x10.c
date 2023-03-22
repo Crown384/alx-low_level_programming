76bbcf683351c4cc5cccda1af95ec3d22d68df0c
Crown384 <crownstephen384@gmail.com>
2023-03-22 03:57:32 +0100

    2-print_alphabet_x10.c$

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,17 +2,16 @@
 
 /**
  * print_alphabet_x10 - Prints x10
- * Return: Always
+ * Return: Always 
  *
  */
-
 
 void print_alphabet_x10(void)
 {
 	char c;
 	int i = 0;
 
-	while (i <= 9);
+	while (i <= 9)
 	{
 	for (c = 'a'; c <= 'z' ; c++)
 	{
