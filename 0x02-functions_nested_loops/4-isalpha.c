4f761459957defd21098e3822e3961785ddfa23c
Crown384 <crownstephen384@gmail.com>
2023-03-22 05:10:56 +0100

    4-isalpha.c$

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _isalpha - jajajajs
+ * @c: chracter to be orjnted
+ * Return: Always
+ */
+
+
+int _isalpha(int c)
+{
+	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
+	{
+	return (1);
+	}
+	return (0);
+{
+
+
+
+
+
+
+
