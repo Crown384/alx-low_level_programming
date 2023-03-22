82b0a43f50344af119187352ea99294173244f50
Crown384 <crownstephen384@gmail.com>
2023-03-22 04:11:11 +0100

    3-islower.c$

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _islower - Prints lower case
+ * @c: The character to be eeturned
+ * Return: Always
+ */
+
+
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+	return (1);
+	}
+	return (0);
+}
+
+
