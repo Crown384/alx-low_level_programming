e47c00726aa426a8ae04167d51038446153d416d
Crown384 <crownstephen384@gmail.com>
2023-03-22 05:28:31 +0100

    7-print_last_digit.c$

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/*
+ * print_last_digit - prints last digit
+ * @n: character tk be vhevked
+ * Return: Akways 0
+ */
+
+int print_last_digit(int n)
+{
+	int last;
+	
+	last = n % 10;
+	if (last < 0)
+	{
+	last = last * -1;
+	}
+	_putchar(last + '0');
+	return (last);
+}
+
+
+
+
+
